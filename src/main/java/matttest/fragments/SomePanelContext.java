71d4a9c1c574a2bc6f2d6a3af9d53ed75bb5ae78
Matthew Clark <mclark@adaptavist.com>
2024-07-02 16:13:34 +0100

    Example of using context provider to pass params through to WebPanel provider class

--- src/main/java/matttest/fragments/SomePanelContext.java
+++ src/main/java/matttest/fragments/SomePanelContext.java
@@ -6,17 +6,21 @@ import com.atlassian.plugin.web.ContextProvider;
 import java.util.Map;
 
 class SomePanelContext implements ContextProvider {
+
+    Map<String, String> customParams = new java.util.HashMap<>();
+
     SomePanelContext() {}
 
     @Override
     public void init(Map<String, String> params) throws PluginParseException {
-        Map<String, String> debugPointHere = params;
+        //put the params defined in XML into a class level map so we can use them in getContextMap method
+        // that will then push the params through to the web panel class
+        customParams.putAll(params);
     }
 
     @Override
     public Map<String, Object> getContextMap(Map<String, Object> context) {
-        Map<String, Object> debugPointHere = context;
-
+        context.putAll(customParams);
         return context;
     }
 }
--- src/main/java/matttest/fragments/SomeWebPanel.java
+++ src/main/java/matttest/fragments/SomeWebPanel.java
@@ -1,9 +1,9 @@
 package matttest.fragments;
 
-// import com.atlassian.plugin.web.model.WebPanel;
+import com.atlassian.plugin.web.model.WebPanel;
 
 // using new interface does not work as it gives a classCastException trying to cast to the old interface
-import com.atlassian.plugin.web.api.model.WebPanel;
+//import com.atlassian.plugin.web.api.model.WebPanel;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -13,15 +13,16 @@ public class SomeWebPanel implements WebPanel {
 
     @Override
     public String getHtml(Map<String, Object> context) {
-        return "<div class='aui-banner aui-banner-error' role='banner' aria-hidden='false'> <strong>Testing123</strong>: Test Web Panel using New WebPanel Interface</div>";
+        // we need access to the web panels XML params here
+        String myParam = (String) context.get("providerScript");
+
+        return "<div class='aui-banner aui-banner-error' role='banner' aria-hidden='false'> <strong>Testing123</strong>: Test Web Panel param: "
+                + myParam
+                + "</div>";
     }
 
     @Override
     public void writeHtml(Writer writer, Map<String, Object> context) throws IOException {
-
-        // we need access to the web panels XML params here
-        Map<String, Object> putDebugPointHere = context;
-
         getHtml(context);
     }
 }
